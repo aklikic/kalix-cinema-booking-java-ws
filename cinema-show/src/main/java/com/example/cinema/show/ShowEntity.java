fe951e4490cf70c7ba75ca7680b29d0e472c842b
Alan <aklikic@gmail.com>
2024-04-14 20:27:29 +0200

    fixes

--- cinema-show/src/main/java/com/example/cinema/show/ShowEntity.java
+++ cinema-show/src/main/java/com/example/cinema/show/ShowEntity.java
@@ -5,6 +5,7 @@ import kalix.javasdk.annotations.EventHandler;
 import kalix.javasdk.annotations.Id;
 import kalix.javasdk.annotations.TypeId;
 import kalix.javasdk.eventsourcedentity.EventSourcedEntity;
+import kalix.javasdk.eventsourcedentity.EventSourcedEntityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,12 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  private String showId;
+
+  public ShowEntity(EventSourcedEntityContext context) {
+    this.showId = context.entityId();
+  }
+
   @Override
   public ShowState emptyState() {
     return ShowState.empty();
@@ -25,12 +32,14 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   @PostMapping
   public Effect<ShowCommandResponse.Ack> createShow(@PathVariable String id, @RequestBody ShowCommand.CreateShow createShow) {
+    logger.info("createShow: showId[{}], createShow[{}]",showId, createShow);
     var result = currentState().handleCommand(id, createShow);
     return handleStateCommandProcessResult(result);
   }
 
   @PatchMapping("/reserve")
   public Effect<ShowCommandResponse.ShowReserveCommandResponse> reserveSeat(@RequestBody ShowCommand.ReserveSeat reserveSeat) {
+    logger.info("reserveSeat: showId[{}], reserveSeat[{}]", showId,reserveSeat);
     if (currentState().isEmpty()) {
       return effects().reply(ShowCommandResponse.ShowReserveCommandResponse.error(ShowCommandError.SHOW_NOT_FOUND));
     }
@@ -40,6 +49,7 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   @PatchMapping("/cancel-reservation/{reservationId}")
   public Effect<ShowCommandResponse.Ack> cancelSeatReservation(@PathVariable String reservationId) {
+    logger.info("cancelSeatReservation: showId[{}], reservationId[{}]", showId,reservationId);
     if (currentState().isEmpty()) {
       return effects().reply(ShowCommandResponse.Ack.error(ShowCommandError.SHOW_NOT_FOUND));
     }
@@ -50,6 +60,7 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   @PatchMapping("/confirm-payment/{reservationId}")
   public Effect<ShowCommandResponse.Ack> confirmSeatReservationPayment(@PathVariable String reservationId) {
+    logger.info("confirmSeatReservationPayment: showId[{}], reservationId[{}]", showId,reservationId);
     if (currentState().isEmpty()) {
       return effects().reply(ShowCommandResponse.Ack.error(ShowCommandError.SHOW_NOT_FOUND));
     }
@@ -60,6 +71,7 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   @GetMapping
   public Effect<ShowCommandResponse.ShowSummeryResponse> get() {
+    logger.info("get: showId[{}]", showId);
     if (currentState().isEmpty()) {
       return effects().reply(ShowCommandResponse.ShowSummeryResponse.error(currentState().id(),ShowCommandError.SHOW_NOT_FOUND));
     } else {
@@ -69,6 +81,7 @@ public class ShowEntity extends EventSourcedEntity<ShowState, ShowEvent> {
 
   @GetMapping("/seat-status/{seatNumber}")
   public Effect<ShowCommandResponse.ShowSeatStatusCommandResponse> getSeatStatus(@PathVariable int seatNumber) {
+    logger.info("get: showId[{}], seatNumber[{}]", showId, seatNumber);
     if (currentState().isEmpty()) {
       return effects().reply(ShowCommandResponse.ShowSeatStatusCommandResponse.error(currentState().id(),seatNumber,ShowCommandError.SHOW_NOT_FOUND));
     } else {
